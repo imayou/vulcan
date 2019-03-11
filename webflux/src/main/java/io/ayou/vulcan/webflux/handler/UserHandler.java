c518553a4dda3999a6c199c3ad18a0466728b87e
ayou <550244300@qq.com>
2019-03-11 18:00:08 +0800

    webclient

--- webflux/src/main/java/io/ayou/vulcan/webflux/handler/UserHandler.java
+++ webflux/src/main/java/io/ayou/vulcan/webflux/handler/UserHandler.java
@@ -4,11 +4,15 @@ import io.ayou.vulcan.webflux.domain.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.client.OkHttp3ClientHttpRequestFactory;
 import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.stereotype.Component;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.reactive.function.server.ServerResponse;
@@ -90,6 +94,68 @@ public class UserHandler {
             Mono<String> stringMono = client.get().retrieve().bodyToMono(String.class);
             stringMono.subscribe(str -> System.out.println(str));
         }
+    }
 
+    public Mono<ServerResponse> login(ServerRequest request) {
+        //jsessionid=k2Jclb2QTVvTqaJB5g7P4CPCB72kQAKv2k1L5nr-kLVgfz1vIsuy!1228397469"
+        MultiValueMap<String, String> formData = new LinkedMultiValueMap<>();
+        formData.add("username", "ex_lidan002");
+        formData.add("password", "123456");
+        formData.add("lt", "LT-1301-FPPAOgw0HOK1Tree0lHOPnM7uOl9bW-CAS.01");
+        formData.add("execution", "e1s1");
+        formData.add("_eventId", "submit");
+        //WebClient client = WebClient.create();
+
+        WebClient client = WebClient.builder()
+                .defaultHeader(HttpHeaders.CONTENT_TYPE, "application/x-www-form-urlencoded")
+                .defaultHeader(HttpHeaders.ACCEPT, "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8")
+                .defaultHeader(HttpHeaders.ACCEPT_ENCODING, "gzip, deflate")
+                .defaultHeader(HttpHeaders.ACCEPT_LANGUAGE, "zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7,en-CA;q=0.6")
+                .defaultHeader(HttpHeaders.CONNECTION, "keep-alive")
+                .defaultHeader(HttpHeaders.USER_AGENT, "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.109 Safari/537.36")
+                .build();
+        Mono<String> stringMono = client.post()
+                //http://10.196.120.4:8080/ipartner/index
+                .uri("http://10.196.20.4:7001/cas/login?service=http%3A%2F%2F10.196.120.4%3A8080%2Fipartner%2Fj_spring_cas_security_check")
+                //.accept(MediaType.APPLICATION_JSON)
+                .cookie("JSESSIONID", "DDtqk8VKV2P04td9SQ68d3aAUZNrtwYe5wsDy-O99jq8q4JRKQzX!-1629022944")
+                .header(HttpHeaders.HOST, "10.196.20.4:7001")
+                .header(HttpHeaders.ORIGIN, "Origin: http://10.196.20.4:7001")
+                .header(HttpHeaders.REFERER, "http://10.196.20.4:7001/cas/login?service=http%3A%2F%2F10.196.120.4%3A8080%2Fipartner%2Fj_spring_cas_security_check")
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                .body(BodyInserters.fromFormData(formData))
+                .retrieve()
+                .bodyToMono(String.class);
+        //System.out.println(stringMono.block());
+
+        return ServerResponse.ok().body(stringMono, String.class);
+    }
+
+    public Mono<ServerResponse> p(ServerRequest request) {
+        MultiValueMap<String, String> formData = new LinkedMultiValueMap<>();
+        formData.add("username", "ex_lidan002");
+        formData.add("password", "123456");
+        formData.add("lt", "LT-605-OcCpr0brGVcI7jkElXlMQT4Tbh3bk1-CAS.01");
+        formData.add("execution", "e1s1");
+        formData.add("_eventId", "submit");
+
+        WebClient client = WebClient.builder()
+                .defaultHeader(HttpHeaders.CONTENT_TYPE, "application/x-www-form-urlencoded")
+                .defaultHeader(HttpHeaders.ACCEPT, "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8")
+                .defaultHeader(HttpHeaders.ACCEPT_ENCODING, "gzip, deflate")
+                .defaultHeader(HttpHeaders.ACCEPT_LANGUAGE, "zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7,en-CA;q=0.6")
+                .defaultHeader(HttpHeaders.CONNECTION, "keep-alive")
+                .defaultHeader(HttpHeaders.USER_AGENT, "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.109 Safari/537.36")
+                .build();
+
+        Mono<String> stringMono = client.get()
+                .uri("http://10.196.120.4:8080/ipartner/index")
+                //.accept(MediaType.APPLICATION_JSON)
+                .cookie("serverInfo", "10.196.120.4:8080")
+                .cookie("JSESSIONID", "C3FFFEEDD0EBE9D45BC1FB2C82873F16")
+                .header(HttpHeaders.HOST, "10.196.120.4:8080")
+                .retrieve()
+                .bodyToMono(String.class);
+        return ServerResponse.ok().body(Mono.just(stringMono), Object.class);
     }
 }
--- webflux/src/main/java/io/ayou/vulcan/webflux/route/UserRouter.java
+++ webflux/src/main/java/io/ayou/vulcan/webflux/route/UserRouter.java
@@ -21,6 +21,8 @@ public class UserRouter {
     public RouterFunction<ServerResponse> responseRouterFunction(UserHandler userHandler) {
         return RouterFunctions.route(RequestPredicates.GET("/api/users"), userHandler::list)
                 .andRoute(RequestPredicates.GET("/api/user/{id}"), userHandler::get)
+                .andRoute(RequestPredicates.GET("/user/login"), userHandler::login)
+                .andRoute(RequestPredicates.GET("/user/login2"), userHandler::p)
                 .andRoute(RequestPredicates.GET("/api/user/{id}/web1"), userHandler::getByWebClient)
                 .andRoute(RequestPredicates.GET("/api/user/{id}/web2"), userHandler::getByWebClient2)
                 .andRoute(RequestPredicates.GET("/api/user/{id}/rest"), userHandler::getByRestTemplate);
